f0194cb5a21cec20e7418f289a12a0fdadf43d3c
Garvit <garvitcpp@gmail.com>
2024-07-05 00:05:21 +0530

    learning data Types

---
+++ dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+
+// alert("Hello") // we are using node js not browser so we dont use this
+
+let name = "Hello boy"
+let age = 18
+let isLoggedIn  = false
+let state = null
+// number
+// bigint
+// string : ""
+// boolean : true/false
+// null : a stadalone value
+// undefined 
+// symbol : unique
+
+// object
+
+console.log(typeof age)
+console.log(typeof state)
+console.log(typeof unefined)
