d1b3db6b596cbd973f3ac8c6776bf938fb2bf6f0
Garvit <garvitcpp@gmail.com>
2024-07-05 16:39:12 +0530

    learning comparisons

--- comparisons.js
+++ comparisons.js
@@ -12,5 +12,7 @@ console.log(null >= 0); // converts null to 0 then checks
 console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
-
+/*
+avoid such conversions
+*/
 console.log("2" === 2);
